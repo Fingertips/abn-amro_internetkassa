79774b090c1493a099d1d50cef7bc35975a39bd2
Eloy Duran <eloy.de.enige@gmail.com>
2009-02-18 16:41:24 +0100

    Added a remote test to verify that we are collecting the appropriate key-value pairs to POST.

--- test/internetkassa_remote_test.rb
+++ test/internetkassa_remote_test.rb
@@ -1,17 +1,55 @@
 require File.expand_path('../test_helper', __FILE__)
 
-xdescribe "AbnAmro::Internetkassa, when remote testing" do
-  it "should create the collect the right values to post" do
-    AbnAmro::Internetkassa.new(
-      :id => 123,
+require 'rest'
+require 'hpricot'
+
+describe "AbnAmro::Internetkassa, when remote testing" do
+  before do
+    @valid_attributes = {
+      :order_id => 123,
       :amount => 1000,
       :description => "HappyHardcore vol. 123 - the ballads"
-    )
+    }
+    @instance = AbnAmro::Internetkassa.new(@valid_attributes)
+  end
+  
+  it "should have the right data to make a successful POST request" do
+    response = post(AbnAmro::Internetkassa.service_url, @instance.data)
+    response.should.be.success
+    
+    parse_response(response).should == {
+      :beneficiary => 'Fingertips BV',
+      :order_id =>    '123',
+      :amount =>      '10.00 EUR'
+    }
   end
   
   private
   
-  def post(url, options)
+  def post(uri, values)
+    body = values.map { |k,v| "#{k}=#{v.to_s.gsub(' ', '%20')}" }.join('&')
+    REST.post(uri, body)
+  end
+  
+  def parse_response(response)
+    results = {}
+    doc = Hpricot(response.body)
     
+    (doc / '#ncol_ref' / 'tr').each do |row|
+      cols = (row / 'td')
+      
+      key = case cols.first.inner_text
+      when /Ordernummer/
+        :order_id
+      when /Totaalbedrag/
+        :amount
+      when /Begunstigde/
+        :beneficiary
+      end
+      
+      results[key] = cols.last.inner_text.strip
+    end
+    
+    results
   end
 end
