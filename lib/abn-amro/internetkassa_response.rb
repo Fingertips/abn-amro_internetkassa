f3c33ff8ee5a316aca1ca168c9c495c9cff9eda9
Eloy Duran <eloy.de.enige@gmail.com>
2009-02-19 17:52:44 +0100

    Return whether or not a payment was successful.

--- lib/abn-amro/internetkassa_response.rb
+++ lib/abn-amro/internetkassa_response.rb
@@ -1,6 +1,13 @@
 module AbnAmro
   class Internetkassa
     class Response
+      def initialize(params)
+        @params = params
+      end
+      
+      def success?
+        @params['STATUS'] == '9'
+      end
     end
   end
 end
--- test/internetkassa_response_test.rb
+++ test/internetkassa_response_test.rb
@@ -4,11 +4,19 @@ describe "AbnAmro::Internetkassa::Response, with a successful payment" do
   before do
     @response = AbnAmro::Internetkassa::Response.new(fixture(:succeeded))
   end
+  
+  it "should be successful" do
+    @response.should.be.success
+  end
 end
 
 describe "AbnAmro::Internetkassa::Response, with a failed payment" do
   before do
     @response = AbnAmro::Internetkassa::Response.new(fixture(:failed))
+  end
+  
+  it "should not be successful" do
+    @response.should.not.be.success
   end
 end
 
@@ -16,10 +24,18 @@ describe "AbnAmro::Internetkassa::Response, with a cancelled payment" do
   before do
     @response = AbnAmro::Internetkassa::Response.new(fixture(:cancelled))
   end
+  
+  it "should not be successful" do
+    @response.should.not.be.success
+  end
 end
 
 describe "AbnAmro::Internetkassa::Response, when an exception occurred" do
   before do
     @response = AbnAmro::Internetkassa::Response.new(fixture(:exception))
+  end
+  
+  it "should not be successful" do
+    @response.should.not.be.success
   end
 end
