336136ff01e5c7e88e1b2a8b42e857382e699fec
Eloy Duran <eloy.de.enige@gmail.com>
2009-02-20 11:01:40 +0100

    Added methods for the essential attributes in a response.

--- lib/abn-amro/internetkassa/response.rb
+++ lib/abn-amro/internetkassa/response.rb
@@ -7,16 +7,27 @@ module AbnAmro
         @params = params
       end
       
+      # attributes
+      
+      def order_id;       @params['orderID']                              end
+      def payment_id;     @params['PAYID']                                end
+      def payment_method; @params['PM']                                   end
+      def acceptance;     @params['ACCEPTANCE']                           end
+      def card_number;    @params['CARDNO']                               end
+      def status_code;    @params['STATUS']                               end
+      def error_code;     @params['NCERROR'] if @params['NCERROR'] != '0' end
+      def client_ip;      @params['IP']                                   end # TODO: double check this is from client
+      def signature;      @params['SHASIGN']                              end
+      def currency;       @params['currency']                             end
+      
+      # methods
+      
       def success?
         error_code.nil?
       end
       
       def retry?
         Codes::ERROR_CODES[error_code][:retry] if error_code
-      end
-      
-      def error_code
-        @params['NCERROR'] unless @params['NCERROR'] == '0'
       end
       
       def error_message
--- test/response_test.rb
+++ test/response_test.rb
@@ -1,5 +1,23 @@
 require File.expand_path('../test_helper', __FILE__)
 
+describe "AbnAmro::Internetkassa::Response, in general" do
+  before do
+    @response = AbnAmro::Internetkassa::Response.new(fixture(:succeeded))
+  end
+  
+  it "should return the essential attributes" do
+    @response.order_id.should == '1235052040'
+    @response.payment_id.should == '3051611'
+    @response.payment_method.should == 'iDEAL'
+    @response.acceptance.should == '0000000000'
+    @response.card_number.should == '11-XXXX-11'
+    @response.status_code.should == '9'
+    @response.client_ip.should == '83.68.2.74'
+    @response.signature.should == '7537DD222E35EE9F9842921BD90C2CBFCFA59466'
+    @response.currency.should == 'EUR'
+  end
+end
+
 describe "AbnAmro::Internetkassa::Response, with a successful payment" do
   before do
     @response = AbnAmro::Internetkassa::Response.new(fixture(:succeeded))
